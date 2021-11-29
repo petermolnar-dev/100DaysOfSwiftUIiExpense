a2a7cf75dff72ec31b2df759ef8f2f1f4384081f
Peter Molnar <petermolnar.hu@gmail.com>
2021-11-29 18:30:25 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Peter Molnar on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
